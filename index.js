13dede26cfdd805bc967eb37d4857ada04bc30d1
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-02-02 18:37:55 +0200

    hf

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const season = 'summer';
 
 if (season === 'winter') {
-    console.log('Zyma');
+  console.log('Zyma');
 
-    [1, 2, 3, 4, 5].forEach(number => {
-        console.log(number);
-    });
+  [1, 2, 3, 4, 5].forEach(number => {
+    console.log(number);
+  });
+} else if (season === 'summer') {
+  console.log('Lito');
+} else if (season === 'autumn') {
+  console.log('Osin');
+} else if (season === 'spring') {
+  console.log('Vesna');
+} else {
+  console.log('shooo blya?');
 }
